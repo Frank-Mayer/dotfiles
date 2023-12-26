dd07f6d342893406520a893e45bc1aed0985f93d
Frank Mayer <gpg@frankmayer.dev>
2023-12-26 01:19:36 +0100

    Sync

--- .config/yab/update.lua
+++ .config/yab/update.lua
@@ -8,7 +8,7 @@ print("Updating Bob")
 os.execute("bob update --all")
 
 print("Updating Nvim")
-os.execute('nvim --headless +"Lazy! sync" +qa')
+os.execute('nvim --headless +"Lazy! sync" +"MasonUpdate" +qa')
 
 print("Updating Go")
 os.execute("go-global-update")
