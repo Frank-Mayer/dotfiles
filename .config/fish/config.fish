650b082d362a3174e53e56ee11c71241c91a4a73
Frank Mayer <gh@frankmayer.dev>
2024-04-16 11:59:12 +0200

    update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,47 +1,58 @@
 set -x LANG en_US.UTF-8
 set -U fish_greeting ""
 
-export CPATH="/opt/homebrew/include:/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/c++/v1/"
-export LDFLAGS="-L/opt/homebrew/opt/gettext/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/gettext/include"
-export LD_LIBRARY_PATH="/opt/homebrew/opt/llvm/include"
+export HOMEBREW_PREFIX="/opt/homebrew"
+export CPATH="$HOMEBREW_PREFIX/include:/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
+export LD_LIBRARY_PATH="$HOMEBREW_PREFIX/opt/llvm/include"
 export GPG_TTY=$(tty)
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_RUNTIME_DIR="$HOME/.local/run"
 export XDG_CONFIG_HOME="$HOME/.config"
-export ANDROID_HOME="$XDG_DATA_HOME"/android
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
-export RUSTC_WRAPPER="/opt/homebrew/bin/sccache"
 export GNUPGHOME="$XDG_DATA_HOME"/gnupg
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
+export PNPM_HOME="$XDG_CONFIG_HOME/pnpm"
 export GOPATH="$XDG_DATA_HOME"/go
 export LESSHISTFILE="$XDG_CACHE_HOME"/less/history
 export NODE_REPL_HISTORY="$XDG_DATA_HOME"/node_repl_history
 
+# lua
+export LUA="$HOMEBREW_PREFIX/opt/luajit/bin/luajit"
+
 export NVM_DIR="$XDG_CONFIG_HOME/nvm"
 
-export GRAPHVIZ_DOT="/opt/homebrew/bin/dot"
+export GRAPHVIZ_DOT="$HOMEBREW_PREFIX/bin/dot"
 
 export BUN_INSTALL="$HOME/.bun"
 
-export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/opt/llvm/bin/:/opt/homebrew/bin:/opt/homebrew/sbin:/opt/homebrew/opt/coreutils/libexec/gnubin:/Applications/WezTerm.app/Contents/MacOS:$GOPATH/bin/:/Users/frank/.local/share/cargo/bin:$HOME/.local/share/nvim/mason/bin:$BUN_INSTALL/bin:/Users/frank/Library/Python/3.9/bin:$PATH"
+export PATH="$GOPATH/bin/:$PNPM_HOME:$HOME/development/flutter/bin:$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$HOMEBREW_PREFIX/opt/llvm/bin/:$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$HOME/.local/share/cargo/bin:$HOME/.local/share/nvim/mason/bin:$BUN_INSTALL/bin:$PATH"
 
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-21.jdk/Contents/Home
 export PATH="$JAVA_HOME/bin:$PATH:/opt/jack/bin"
 
 alias gcc=gcc-13
 alias g++=g++-13
+export RUSTC_WRAPPER=$(which sccache)
 
 # bob
-export PATH="$XDG_DATA_HOME/bob/nvim-bin:$PATH"
+export PATH="$PATH:$XDG_DATA_HOME/bob/nvim-bin"
 
 alias py=python3
 alias python=python3
 alias pip=pip3
 export PYTHON=python3
+
+alias lua=luajit
+
+# ruby
+export GEM_HOME="$XDG_DATA_HOME/gem"
+export PATH="$GEM_HOME/bin:$HOMEBREW_PREFIX/opt/ruby/bin:$PATH"
+set -gx LDFLAGS "-L$HOMEBREW_PREFIX/opt/ruby/lib -L$HOMEBREW_PREFIX/opt/gettext/lib"
+set -gx CPPFLAGS "-I$HOMEBREW_PREFIX/opt/ruby/include -I$HOMEBREW_PREFIX/opt/gettext/include"
+set -gx PKG_CONFIG_PATH "$HOMEBREW_PREFIX/opt/ruby/lib/pkgconfig"
 
 function gohere
     mkdir -p "$argv"
@@ -59,12 +70,6 @@ function gohere
     go mod tidy
     curl https://raw.githubusercontent.com/github/gitignore/main/Go.gitignore -o .gitignore
     echo "# $argv" > README.md
-    echo "" >> README.md
-    echo "## Install" >> README.md
-    echo "" >> README.md
-    echo "```bash" >> README.md
-    echo "go install github.com/Frank-Mayer/$argv@latest" >> README.md
-    echo "```" >> README.md
     git init
 end
 
@@ -98,7 +103,7 @@ alias gtree="git log --oneline --graph --color --all --decorate"
 
 alias vim="nvim"
 alias vi="nvim"
-export VISUAL=nvim
+export VISUAL=$(which zed)
 export EDITOR=$(which nvim)
 export GIT_EDITOR=$(which nvim)
 
